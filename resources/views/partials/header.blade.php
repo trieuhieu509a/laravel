a9399355b8de92a054e684952fbaa076feaa7fde
trieuhieu509a@gmail.com <prettyboy1>
2018-03-23 17:51:24 +0700

    part 5

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="{{ route('blog.index') }}" class="navbar-brand">Laravel Guide</a>
+            <ul class="nav navbar-nav">
+                <li><a href="{{ route('blog.index') }}">Blog</a></li>
+                <li><a href="{{ route('other.about') }}">About</a></li>
+                @if(!Auth::check())
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                @else
+                    <li><a href="{{ route('admin.index') }}">Posts</a></li>
+                    <li>
+                        <a href="{{ url('/logout') }}"
+                           onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+
+                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
